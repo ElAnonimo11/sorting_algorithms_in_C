2f0fafa10cb8691ba64e5585fc07819d06628ad3
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-08 11:57:47 +0200

    Update bubble_sort.c

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -5,6 +5,7 @@ void bubble_sort(int* vector, int tam_vector)
 {
     for (int i = tam_vector - 1; i >= 0 ; --i) {
         for (int j = 0; j < i ; ++j) {
+            // The big numbers go to the top of the array, like a bubble in a liquid
             if (vector[j] > vector[j+1]) {
                 int aux = vector[j];
                 vector[j] = vector[j+1];
