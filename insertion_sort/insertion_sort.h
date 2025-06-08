db54257784ab004cde3396ae96ee488959e4e738
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-08 12:16:13 +0200

    Update insertion_sort.h

--- insertion_sort/insertion_sort.h
+++ insertion_sort/insertion_sort.h
@@ -1,6 +1,11 @@
 #ifndef _INSERTION_SORT_H_
 #define _INSERTION_SORT_H_
 
+/*
+ * In insertion sort, we search for each element's position
+ * at the beginning of the already sorted array.
+ */
+
 void insertion_sort(int vector[],int tam_vector);
 
 #endif
